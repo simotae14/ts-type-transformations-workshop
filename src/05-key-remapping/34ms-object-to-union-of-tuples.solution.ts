13dce320df476f5b9502d70a7ef8a2eff2040b28
Simone Taeggi <startae14@gmail.com>
2023-03-24 12:32:13 +0100

    exercise Map an object into a Discriminated Union

--- src/05-key-remapping/34ms-object-to-union-of-tuples.solution.ts
+++ src/05-key-remapping/34ms-object-to-union-of-tuples.solution.ts
@@ -7,8 +7,8 @@ interface Values {
 }
 
 type ValuesAsUnionOfTuples = {
-  [K in keyof Values]: [K, Values[K]];
-};
+  [K in keyof Values]: [K, Values[K]]
+}[keyof Values];
 
 type tests = [
   Expect<
